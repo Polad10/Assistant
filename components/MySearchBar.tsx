f9fd14fb9c5052b3990a198b48144f9344c89a13
Polad <samadzada.polad@gmail.com>
2022-12-11 23:00:52 +0100

    MySearchBar component

---
+++ components/MySearchBar.tsx
@@ -0,0 +1,40 @@
+import { SetStateAction, useState } from 'react';
+import { useTheme } from '@react-navigation/native';
+import { Platform, StyleSheet } from 'react-native';
+import { SearchBar } from '@rneui/themed';
+import { Colors } from '../types/Colors';
+
+export default function MySearchBar() {
+  const { colors } = useTheme();
+  const [search, setSearch] = useState('');
+
+  function updateSearch(value: SetStateAction<string>) {
+    setSearch(value);
+  }
+
+  return (
+    <SearchBar
+      placeholder='Search...'
+      platform={Platform.OS === 'ios' || Platform.OS === 'android' ? Platform.OS : 'default'}
+      lightTheme={false}
+      containerStyle={styles(colors).searchBarContainer}
+      inputContainerStyle={styles(colors).searchBarInputContainer}
+      inputStyle={styles(colors).searchBarInput}
+      value={search}
+      onChangeText={updateSearch}
+    />
+  );
+}
+
+const styles = (colors: Colors) =>
+  StyleSheet.create({
+    searchBarContainer: {
+      backgroundColor: colors.background,
+    },
+    searchBarInputContainer: {
+      backgroundColor: colors.card,
+    },
+    searchBarInput: {
+      color: colors.text,
+    },
+  });
--- components/Patients.tsx
+++ components/Patients.tsx
@@ -1,46 +1,14 @@
-import { useTheme } from '@react-navigation/native';
-import { View, Platform, StyleSheet } from 'react-native';
-import { SearchBar } from '@rneui/themed';
-import { Colors } from '../types/Colors';
-import { SetStateAction, useState } from 'react';
+import { View } from 'react-native';
 import PatientGroup from './PatientGroup';
 import MyFAB from './MyFAB';
+import MySearchBar from './MySearchBar';
 
 export default function Clients({ navigation }: any) {
-  const { colors } = useTheme();
-  const [search, setSearch] = useState('');
-
-  function updateSearch(value: SetStateAction<string>) {
-    setSearch(value);
-  }
-
   return (
     <View style={{ flex: 1 }}>
-      <SearchBar
-        placeholder='Search...'
-        platform={Platform.OS === 'ios' || Platform.OS === 'android' ? Platform.OS : 'default'}
-        lightTheme={false}
-        containerStyle={styles(colors).searchBarContainer}
-        inputContainerStyle={styles(colors).searchBarInputContainer}
-        inputStyle={styles(colors).searchBarInput}
-        value={search}
-        onChangeText={updateSearch}
-      />
+      <MySearchBar />
       <PatientGroup />
       <MyFAB onPress={() => navigation.navigate('NewPatient')} />
     </View>
   );
 }
-
-const styles = (colors: Colors) =>
-  StyleSheet.create({
-    searchBarContainer: {
-      backgroundColor: colors.background,
-    },
-    searchBarInputContainer: {
-      backgroundColor: colors.card,
-    },
-    searchBarInput: {
-      color: colors.text,
-    },
-  });
