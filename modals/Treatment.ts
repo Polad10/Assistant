74a3ffec0cbbb39abd514db8bd931cc1e6114ea2
Polad <samadzada.polad@gmail.com>
2023-12-05 19:40:22 +0100

    remove finished property

--- modals/Treatment.ts
+++ modals/Treatment.ts
@@ -8,12 +8,10 @@ interface BaseTreatment {
 
 interface Treatment extends BaseTreatment {
   id: number
-  finished: boolean
 }
 
 interface TreatmentRequest extends BaseTreatment {
   id?: number | null | undefined
-  finished?: boolean | null | undefined
 }
 
 export { Treatment, TreatmentRequest }
