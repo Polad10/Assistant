bc9678049bd54c25754e33c68f1590ac8823bf5e
Polad <samadzada.polad@gmail.com>
2023-12-26 14:37:18 +0100

    improve treatment list item

--- components/TreatmentItem.tsx
+++ components/TreatmentItem.tsx
@@ -1,15 +1,16 @@
-import { View, StyleSheet, DeviceEventEmitter } from 'react-native'
-import { ListItem } from '@rneui/themed'
+import { View, StyleSheet, DeviceEventEmitter, Text } from 'react-native'
+import { Chip, ListItem } from '@rneui/themed'
 import { Colors } from '../types/Colors'
 import { useNavigation, useTheme } from '@react-navigation/native'
 import { TouchableHighlight } from 'react-native-gesture-handler'
 import { RootStackParamList, RootStackScreenProps } from '../types/Navigation'
 import { getPatientFullName } from '../helpers/PatientHelper'
-import { DateTime } from 'luxon'
 import { useContext } from 'react'
 import { DataContext } from '../contexts/DataContext'
 import { Treatment } from '../modals/Treatment'
 import { treatmentFinished } from '../helpers/TreatmentHelper'
+import IonIcons from '@expo/vector-icons/Ionicons'
+import { Status } from '../enums/Status'
 
 export type TreatmentItemProps = {
   treatment: Treatment
@@ -32,25 +33,28 @@ export default function TreatmentItem(props: TreatmentItemProps) {
   }
 
   const patient = context?.patients?.find((p) => p.id === props.treatment.patient_id)
+  const status = styleProps.finished ? Status.FINISHED : Status.ONGOING
 
   return (
     <TouchableHighlight onPress={() => handleTreatmentSelect(props.treatment)}>
       <ListItem containerStyle={styles(styleProps).listItemContainer}>
         <ListItem.Content>
           <ListItem.Title style={styles(styleProps).listItemTitle}>{props.treatment.title}</ListItem.Title>
-          <ListItem.Subtitle style={styles(styleProps).listItemSubtitle}>
-            Patient: {getPatientFullName(patient)}
-          </ListItem.Subtitle>
           <View style={styles(styleProps).listItemStatus}>
-            <ListItem.Subtitle style={styles(styleProps).listItemSubtitle}>
-              Start date: {DateTime.fromISO(props.treatment.start_date).toISODate()}
-            </ListItem.Subtitle>
             <View style={styles(styleProps).listItemRow}>
-              <ListItem.Subtitle style={styles(styleProps).listItemSubtitle}>Status: </ListItem.Subtitle>
-              <ListItem.Subtitle style={[styles(styleProps).listItemSubtitle, styles(styleProps).statusColor]}>
-                {styleProps.finished ? 'Finished' : 'Ongoing'}
+              <ListItem.Subtitle style={[styles(styleProps).listItemSubtitle, styles(styleProps).infoIcon]}>
+                <IonIcons name='person-outline' size={14} />
+              </ListItem.Subtitle>
+              <ListItem.Subtitle style={[styles(styleProps).listItemSubtitle, styles(styleProps).patient]}>
+                {getPatientFullName(patient)}
               </ListItem.Subtitle>
             </View>
+            <Chip
+              title={status}
+              type='outline'
+              titleStyle={styles(styleProps).status}
+              buttonStyle={styles(styleProps).statusButton}
+            />
           </View>
         </ListItem.Content>
       </ListItem>
@@ -80,14 +84,24 @@ const styles = (styleProps: StyleProps) =>
       opacity: 0.5,
     },
     listItemStatus: {
-      flexDirection: 'row',
       width: '100%',
+      flexDirection: 'row',
       justifyContent: 'space-between',
     },
     listItemRow: {
       flexDirection: 'row',
+      alignItems: 'center',
     },
-    statusColor: {
+    status: {
       color: styleProps.finished ? 'lightgreen' : 'orange',
+    },
+    statusButton: {
+      borderColor: styleProps.finished ? 'lightgreen' : 'orange',
+    },
+    infoIcon: {
+      marginRight: 10,
+    },
+    patient: {
+      opacity: 0.6,
     },
   })
