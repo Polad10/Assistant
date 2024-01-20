e75a86765216812f437b6a578e09b35a47605d25
Polad <samadzada.polad@gmail.com>
2024-01-20 02:36:56 +0100

    new patient loading view

--- components/NewPatient.tsx
+++ components/NewPatient.tsx
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useEffect } from 'react'
+import { useCallback, useContext, useEffect, useState } from 'react'
 import PatientForm from './PatientForm'
 import { DeviceEventEmitter } from 'react-native'
 import { PatientRequest } from '../modals/Patient'
@@ -6,6 +6,8 @@ import { DataContext } from '../contexts/DataContext'
 import { useNavigation } from '@react-navigation/native'
 import { RootStackScreenProps } from '../types/Navigation'
 import { showSuccessMessage } from '../helpers/ToastHelper'
+import MainView from './MainView'
+import LoadingView from './LoadingView'
 
 export default function NewPatient() {
   const navigation = useNavigation<RootStackScreenProps<'NewPatient'>['navigation']>()
@@ -15,8 +17,13 @@ export default function NewPatient() {
     return
   }
 
+  const [loading, setLoading] = useState(false)
+
   const handlePatientSave = useCallback(async (patient: PatientRequest) => {
+    setLoading(true)
     const newPatient = await context.createPatient(patient)
+    setLoading(false)
+
     showSuccessMessage('Patient added')
 
     if (DeviceEventEmitter.listenerCount('patientCreated') > 0) {
@@ -34,5 +41,10 @@ export default function NewPatient() {
     }
   }, [])
 
-  return <PatientForm pageName='NewPatient' />
+  return (
+    <MainView>
+      <PatientForm pageName='NewPatient' />
+      {loading && <LoadingView />}
+    </MainView>
+  )
 }
